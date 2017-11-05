5cba3e250669bc4df84c98ff50bc8c84cd79d177
ManuelMedina88 <anglane10@gmail.com>
2017-11-04 22:17:21 -0400

    I almost finish level 3

---
+++ masteringLevel3/Increment.java
@@ -0,0 +1,24 @@
+// Increment.java
+// @uthor : Manuel Medina.
+// Date nov 4 2017.
+// Prefix increment and postfix increment operators.
+
+public class Increment
+{
+   public static void main(String[] args)
+   {
+      // demonstrate postfix increment operator
+      int c = 5;
+      System.out.printf("c before postincrement: %d%n", c); // prints 5
+      System.out.printf("     posticrementing c: %d%n", c++); // prints 5
+      System.out.printf(" c after postincrement: %d%n", c); // prints 6
+
+      System.out.println(); // skip a line
+
+      // demonstrate prefix increment operator
+      c = 5;
+      System.out.printf(" c before preincrement: %d%n", c); // prints 5
+      System.out.printf("     preincrementing c: %d%n", ++c); // prints 6
+      System.out.printf("  c after preincrement: %d%n", c); // prints 6
+   }
+} // end class Increment
